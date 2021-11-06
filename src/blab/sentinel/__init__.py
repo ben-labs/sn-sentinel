73b4f9fdfacf7d479514b38595d569fa3517ffee
Ben <ben.quek@ben-labs.net>
2021-11-06 22:00:03 +0800

    added in the telegram class

--- src/blab/sentinel/__init__.py
+++ src/blab/sentinel/__init__.py
@@ -1,4 +1,5 @@
 from ftplib import FTP, error_perm
+import requests
 
 
 class Ftp:
@@ -44,3 +45,37 @@ class Ftp:
         if self.get_patches(filter):
             return self._patches
         return []
+
+
+class Telegram:
+    '''Handles telegram functions'''
+
+    def __init__(self, token):
+        """
+        Initialize Token
+        """
+        self.last_processed_message = 0
+        self.req = f"https://api.telegram.org/bot{token}/"
+
+    def __request(self, resource, **kwargs):
+        """
+        Process Telegram API Request
+        """
+        if "data" in kwargs:
+            r = requests.post(f"{self.req}{resource}", kwargs["data"]).json()
+        else:
+            r = requests.post(f"{self.req}{resource}").json()
+        return r
+
+    def send_message(self, chat_id, text, parse_mode='html'):
+        """
+        Sends a telegram message
+        Can't send a message to a person if they do not already have an initial chat with you.
+        """
+        payload = {
+            "chat_id": chat_id,
+            "text": text,
+            "parse_mode": parse_mode
+        }
+        result = self.__request("sendMessage", data=payload)
+        return result
